01aa4fc02c28abf5097b44d5d6f223375a77b6f9
gmerville-jehann <gmerville@jehann.fr>
2023-10-31 16:41:40 +0100

    Created ping method

--- src/main/java/io/getarrays/server/resource/ServerResource.java
+++ src/main/java/io/getarrays/server/resource/ServerResource.java
@@ -1,16 +1,21 @@
 package io.getarrays.server.resource;
 
+import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Response;
+import io.getarrays.server.model.Server;
 import io.getarrays.server.service.implementation.ServerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Map;
 
+import static io.getarrays.server.enumeration.Status.*;
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -26,10 +31,25 @@ public class ServerResource {
             Response.builder()
                     .timeStamp(now())
                     .data(Map.of("servers", serverService.list(30)))
-                    .message("")
+                    .message("Servers retrieved")
                     .status(OK)
                     .statusCode(OK.value())
                     .build()
+        );
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServers(@PathVariable("ipAddress") String ipAddress)
+            throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("server", server))
+                        .message(server.getStatus() == SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
         );
     }
 }
