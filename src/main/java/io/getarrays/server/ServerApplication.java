cca043cce7668b5c79fcb862d427bc9a3ffc7848
gmerville-jehann <gmerville@jehann.fr>
2023-10-31 17:00:37 +0100

    Created command line method

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,7 +1,14 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static io.getarrays.server.enumeration.Status.*;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +17,13 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/images/icon1.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16 GB", "Dell Tower", "http://localhost:8080/server/images/icon2.png", SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32 GB", "Web Server", "http://localhost:8080/server/images/icon3.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "Red Hat Enterprise Linux", "64 GB", "Mail Server", "http://localhost:8080/server/images/icon4.png", SERVER_DOWN));
+		};
+	}
 }
