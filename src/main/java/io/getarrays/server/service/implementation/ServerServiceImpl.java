42714080c89bb5c49b970587b054724c205ddde1
gmerville-jehann <gmerville@jehann.fr>
2023-10-31 16:08:47 +0100

    Finished list method

--- src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/io/getarrays/server/service/implementation/ServerServiceImpl.java
@@ -1,18 +1,19 @@
 package io.getarrays.server.service.implementation;
 
-import io.getarrays.server.enumeration.Status;
 import io.getarrays.server.model.Server;
 import io.getarrays.server.repository.ServerRepository;
 import io.getarrays.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Collection;
+
+import static io.getarrays.server.enumeration.Status.*;
 
 @Service
 @Transactional
@@ -33,14 +34,15 @@ public class ServerServiceImpl implements ServerService {
         log.info("Pinging server IP: {}", ipAddress);
         Server server = serverRepository.findByIpAddress(ipAddress);
         InetAddress address = InetAddress.getByName(ipAddress);
-        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
         serverRepository.save(server);
         return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepository.findAll(PageRequest.of(0,limit)).toList();
     }
 
     @Override
